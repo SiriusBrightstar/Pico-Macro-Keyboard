def91be09213a28029ceedc8b3e6222d0a5c2494
SiriusBrightstar <62252266+SiriusBrightstar@users.noreply.github.com>
2021-03-02 20:02:18 +0530

    Add files via upload

---
+++ Macro_code.py
@@ -0,0 +1,56 @@
+import time
+import board
+import digitalio
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+
+kbd = Keyboard(usb_hid.devices)             #Initialize Pico as a HID Keyboard Device
+layout = KeyboardLayoutUS(kbd)
+
+LED = digitalio.DigitalInOut(board.GP25)    #Initialize On-Board LED
+LED.direction = digitalio.Direction.OUTPUT
+
+BT1 = digitalio.DigitalInOut(board.GP15)    #Set GPIO 15 as Button 1
+BT1.direction = digitalio.Direction.INPUT   #Set GPIO 15 as Input 
+BT1.pull = digitalio.Pull.DOWN              #Set GPIO 15 as Active HIGH, meaning GP15 is LOW & to be 
+                                            #triggered 3.3v needs to be connected. 
+
+BT2 = digitalio.DigitalInOut(board.GP14)    #Set GPIO 14 as Button 
+BT2.direction = digitalio.Direction.INPUT
+BT2.pull = digitalio.Pull.DOWN
+
+BT3 = digitalio.DigitalInOut(board.GP13)    #Set GPIO 13 as Button 
+BT3.direction = digitalio.Direction.INPUT
+BT3.pull = digitalio.Pull.DOWN
+
+BT4 = digitalio.DigitalInOut(board.GP12)    #Set GPIO 12 as Button 
+BT4.direction = digitalio.Direction.INPUT
+BT4.pull = digitalio.Pull.DOWN
+
+BT5 = digitalio.DigitalInOut(board.GP11)    #Set GPIO 11 as Button 
+BT5.direction = digitalio.Direction.INPUT
+BT5.pull = digitalio.Pull.DOWN
+
+while True:                                 #Infinite Loop
+    if BT1.value == True:
+        layout.write('aspirine')            #Add text to be triggered by GP15
+        LED.value = True                    #LED turns on for 0.1s when Button is pressed
+        time.sleep(0.1)
+    elif BT2.value == True:
+        layout.write('preciousprotection')  #Add text to be triggered by GP14
+        LED.value = True
+        time.sleep(0.1)
+    elif BT3.value == True:
+        layout.write('leavemealone')        #Add text to be triggered by GP13
+        LED.value = True
+        time.sleep(0.1)
+    elif BT4.value == True:
+        layout.write('panzer')              #Add text to be triggered by GP12
+        LED.value = True
+        time.sleep(0.1)
+    elif BT5.value == True:
+        layout.write('gettherefast')        #Add text to be triggered by GP11
+        LED.value = True
+        time.sleep(0.1)
+    LED.value = False
